d3cc4c14d879eded1c3b6c8de8177d44051374d0
Saketh Gajawada <sakethgajawada21@gmail.com>
2021-03-20 16:12:05 +0000

    updated gui

--- gui.py
+++ gui.py
@@ -6,7 +6,8 @@ name_var = tk.StringVar()
 email_id_var = tk.StringVar()
 vaccine_status_var = tk.BooleanVar()
 covid_status_var = tk.BooleanVar()
-
+contacts_name_lst = []
+contacts_email_id_lst = []
 #function when submit button is clicked
 def submit():
   name = name_var.get()
@@ -14,20 +15,24 @@ def submit():
   vaccine_status = vaccine_status_var.get()
   covid_status = covid_status_var.get()
   display = tk.Tk()
-  add_contacts_bt = tk.Button(display,text = "Add",command = add).pack()
+  def add():
+    add_contacts = tk.Tk()
+    add_name_var = tk.StringVar()
+    add_email_id_var = tk.StringVar()
+    #name
+    c_name_lb = tk.Label(add_contacts,text = "Name", font=("Times New Roman", 12), bg="white").place(x=60,y=400)
+    name_entry = tk.Entry(add_contacts,textvariable = add_name_var, font=("Times New Roman", 12), bg="white").place(x=280,y=400)
+    #email
+    c_email_id_lb = tk.Label(add_contacts,text = "Email ID ", font=("Times New Roman", 12), bg="white").place(x=60,y=430)
+    c_email_id_entry = tk.Entry(add_contacts,textvariable = add_email_id_var, font=("Times New Roman", 12), bg="white").place(x=280,y=430)
+    def add_button():
+      contacts_name_lst.append(add_name_var.get())
+      contacts_email_id_lst.append(add_email_id_var.get())
+      add_contacts.destroy()
+    c_submit = tk.Button(text = "Add",command = add_button).place(x = 100,y = 500)
+    add_contacts.mainloop()
+  add_contacts_bt = tk.Button(display, text="Add", command=add).pack()
   window.destroy()
-def add():
-  add_contacts = tk.Tk()
-  add_name_var = tk.StringVar()
-  add_email_id_var = tk.StringVar()
-   #name
-  c_name_lb = tk.Label(add_contacts,text = "Name", font=("Times New Roman", 12), bg="white").place(x=60,y=400)
-  name_entry = tk.Entry(add_contacts,textvariable = add_name_var, font=("Times New Roman", 12), bg="white").place(x=280,y=400)
-  #email
-  c_email_id_lb = tk.Label(add_contacts,text = "Email ID ", font=("Times New Roman", 12), bg="white").place(x=60,y=430)
-  c_email_id_entry = tk.Entry(add_contacts,textvariable = add_email_id_var, font=("Times New Roman", 12), bg="white").place(x=280,y=430)
-  c_submit = tk.Button(text = "Add",command = "function to display name and email id in the list and add this line to it add_contacts.destroy() at last ").pack()
-
 #name
 name_lb = tk.Label(window,text = "Name", font=("Times New Roman", 12), bg="white").place(x=60,y=400)
 name_entry = tk.Entry(window,textvariable = name_var, font=("Times New Roman", 12), bg="white").place(x=280,y=400)
@@ -45,3 +50,5 @@ vaccine_r2 = tk.Radiobutton(window, text="No", variable=vaccine_status_var, valu
 #submit button
 submit_bt = tk.Button(window,text = "SUBMIT",fg="white", bg="black",command = submit).place(x = 100,y = 600)
 window.mainloop()
+print(contacts_name_lst)
+print(contacts_email_id_lst)

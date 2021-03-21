f87f640b28001eda072cfbe918547602c8a0fe93
Sougandh Krishna K S <72473176+SougandhKrishna@users.noreply.github.com>
2021-03-21 20:33:35 +0530

    Update Amazon_Price_Tracker.py
    Adding comments and minor changes(Changed URL and price)

--- Amazon_Price_Tracker.py
+++ Amazon_Price_Tracker.py
@@ -1,12 +1,14 @@
+#Importing modules
 import requests
 from bs4 import BeautifulSoup
 from pushbullet import PushBullet
 from threading import Timer
 
-
-URL = 'https://www.amazon.in/Apple-iPhone-8-Gold-256GB/dp/B071P3764Z/ref=sr_1_2?crid=3REL7USFBYB3I&dchild=1&keywords=apple+i+phone7s+plus&qid=1616307855&sprefix=apple+i+p%2Caps%2C325&sr=8-2'
-desired_price=50000
-API_KEY = 'o.8UljdagUxLKUyT0IEhPHxKHSxHCYMo59'
+URL = 'https://www.amazon.in/New-Apple-iPhone-Pro-128GB/dp/B08L5VZKWT/ref=sr_1_3?dchild=1&keywords=iphone&qid=1616338851&sr=8-3'
+                                #URL of the product page(On Amazon)
+desired_price=130000             #Price below which you should be notified
+API_KEY = 'o.8UljdagUxLKUyT0IEhPHxKHSxHCYMo59'  #Access Token from pushbullet
+interval=1                      #Enter value in hours(If interval=2 the program checks the price every 2 hours)
 
 
 headers = { 
@@ -17,33 +19,40 @@ headers = {
 'DNT' : '1', # Do Not Track Request Header 
 'Connection' : 'close'
 }
+                                                #Web Scraping
 
 
-def notification(product_name,product_price):
+
+def notification(product_name,product_price):   #Function which is used to send notification on our phone
 
     text=product_name
     text+=' is now available for your desired price.'
     text+='\n You can get it now at '
     text+=product_price
     text+='\n'
-    text+=URL
+    text+=URL                                   #Text contains the message which will be displayed as notification
     
-    pb=PushBullet(API_KEY)
+    pb=PushBullet(API_KEY)                      #Class Variable of API_KEY
     
-    push = pb.push_note('Price Drop',text)
+    push = pb.push_note('Price Drop',text)      #Sending Notification
+                                                #Title of Notification followed by text inside the parenthesis
 
-    print('Notification Sent!')
+    print('Notification Sent!')                 #Prints 'Notification Sent' on your terminal
 
 
 def check_price():
     page = requests.get(URL,headers=headers)
     soup = BeautifulSoup(page.content,'html.parser')
 
-    product_name = soup.find(id='productTitle').get_text()
-    product_name = str(product_name)
-    product_name = product_name.strip()
+    product_name = soup.find(id='productTitle').get_text()  #Finds id called productTitle which contains
+                                                            #the name of the product on Amazon Page
+    product_name = str(product_name)                        #Converts it into text
+    product_name = product_name.strip()                     #It has a lot of extra spaces so removing them
     #print(product_name)
 
+
+#In some pages price of the product is stored in id='priceblock_ourprice' and in some it is stored in id='priceblock_dealprice'
+#So taking care of both
     a=soup.find(id='priceblock_dealprice')
     if(a==None):
         product_price1 = soup.find(id='priceblock_ourprice').get_text()
@@ -54,19 +63,22 @@ def check_price():
     product_price =''
     for letters in product_price1:
         if letters.isnumeric() or letters =='.':
-            product_price+=letters
+            product_price+=letters                    
     product_price=float(product_price)
+                                        #Price contains the rupee symbol so removing that and making it a float
+
     #print(product_price)
 
-    if product_price<=desired_price:
+    if product_price<=desired_price:    
         print("Price of the Product reached below the Desired Price.")
         notification(product_name,product_price1)
         return
     else:
         print("Price of the Product is still more than the Desired Price.")
+                                        #This blocks compares the price and calls the notification function if necessary
     
-    Timer(60*60,check_price).start()
+    Timer(interval*60*60,check_price).start()
+                                        #This statement controls the time after which loop is run again
+                                        #That is the time gap after which we should check the price
 
 check_price()
-
-
